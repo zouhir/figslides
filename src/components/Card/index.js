9dd65f9795fb5bd9d7751f15bca80a79977e123d
Zouhir <zouhir@zouhir.org>
2019-05-18 11:56:53 -0700

    cleanup default cli template

--- src/components/Card/index.js
+++
@@ -1,9 +0,0 @@
-import style from './index.sass';
-
-export default function (props) {
-	return (
-		<div class={ style.card }>
-			{ props.children }
-		</div>
-	);
-}
--- src/components/Intro/index.js
+++
@@ -1,9 +0,0 @@
-import style from './index.sass';
-
-export default function (props) {
-	return (
-		<header class={ style.intro }>
-			{ props.children }
-		</header>
-	);
-}
